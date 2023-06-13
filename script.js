2fc2277d0936fc91b101fae2f12c6cfbe21985f9
Nadezhda Maul <maulnadezda@gmail.com>
2023-06-13 16:48:38 +0300

    2

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function calcSum() {
     let rub = '₽';
     const price = Number(document.querySelector('.price').value);
-    const payment1 = Number(document.querySelector('.payment1').value);
+    const payment_first = Number(document.querySelector('.payment_first').value);
 
-    const sum = price - payment1;
-    document.querySelector('.result1').innerHTML = Number(sum) + ' ' + rub;
+    const sum = price - payment_first;
+    document.querySelector('.calc_first').innerHTML = Number (sum) + ' ' + rub;
 }
 
 function calcPayment() {
     let rub = '₽';
     let rate = 6.9;
     const price = Number(document.querySelector('.price').value);
-    const payment1 = Number(document.querySelector('.payment1').value);
-    const sum = price - payment1;
+    const payment_first = Number(document.querySelector('.payment_first').value);
+    const sum = price - payment_first;
     const period = Number(document.querySelector('.period').value)
 
     const i = (rate / 12) / 100;
     const k = (i * (Math.pow(1 + i, period * 12))) / (Math.pow(1 + i, period * 12) - 1);
     const result = sum * k;
 
-    document.querySelector('.result2').innerHTML = Number(result).toFixed(0) + ' ' + rub;
+    document.querySelector('.calc_second').innerHTML = Number(result).toFixed(0) + ' ' + rub;
 
-    const resultElement = document.querySelector('.result2');
+    const resultElement = document.querySelector('.calc_second');
     resultElement.classList.remove('result--red');
 
     if (result > 50000) {
-        resultElement.classList.add('result--red'); 
+        resultElement.classList.add('result--red');
 }
 }
 
